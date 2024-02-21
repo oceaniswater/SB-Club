2712b522918e71faf6e615adf338f0451dbf5bb0
oceaniswater <markflyingbear@gmail.com>
2024-02-21 15:07:14 +0000

    Initial Commit

---
+++ SB Club/SB_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SB_ClubApp.swift
+//  SB Club
+//
+//  Created by Mark Golubev on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SB_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
