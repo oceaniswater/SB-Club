6e69f92cd32e88c9214e4295f1652411147a5516
oceaniswater <markflyingbear@gmail.com>
2024-02-21 17:28:55 +0000

    fix tabbar

--- SB Club/AppTabView.swift
+++ SB Club/AppTabView.swift
@@ -10,25 +10,28 @@ import SwiftUI
 struct AppTabView: View {
     var body: some View {
         TabView {
-            MembershipView()
-                .tabItem {
-                    Label("Membership", systemImage: "person")
-                }
-            
-            BookCourtView()
-                .tabItem {
-                    Label("Book a Court", systemImage: "figure.tennis")
-                }
-            
-            MembershipView()
-                .tabItem {
-                    Label("Book Sessions", systemImage: "figure.cooldown")
-                }
-            
-            OtherView()
-                .tabItem {
-                    Label("Other", systemImage: "square.on.square.badge.person.crop.fill")
-                }
+            Group {
+                MembershipView()
+                    .tabItem {
+                        Label("Membership", systemImage: "person")
+                    }
+                
+                BookCourtView()
+                    .tabItem {
+                        Label("Book a Court", systemImage: "figure.tennis")
+                    }
+                
+                BookSessionsView()
+                    .tabItem {
+                        Label("Book Sessions", systemImage: "figure.cooldown")
+                    }
+                
+                OtherView()
+                    .tabItem {
+                        Label("Other", systemImage: "square.on.square.badge.person.crop.fill")
+                    }
+            }
+            .toolbarBackground(.visible, for: .tabBar)
             
             
         }
